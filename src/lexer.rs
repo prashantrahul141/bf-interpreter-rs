d11238cb5e5a9be5befb7afc9b8df32cdf8e5ba2
Prashant <prashantrahul141@protonmail.com>
2024-02-14 01:48:13 +0530

    implement scanning of tokens for lexer.

--- src/lexer.rs
+++ src/lexer.rs
@@ -15,4 +15,77 @@ impl Lexer {
         let tokens = Lexer::scan_tokens(input);
         Self { tokens }
     }
+
+    /// Scans a string into Vec of Tokens.
+    /// # Arguments
+    /// * input - Immutable reference to the string to scan.
+    /// # Returns
+    /// * tokens - Vector of Tokens scanned.
+    fn scan_tokens(input: &String) -> Vec<Token> {
+        let mut line = 0;
+
+        input
+            // taking characters of strings.
+            .chars()
+            // mapping through it.
+            .flat_map(|lexeme| match lexeme {
+                // if the character is >
+                '>' => Some(Token {
+                    line,
+                    token_type: TokenType::RightAngle,
+                }),
+
+                // if the character is <
+                '<' => Some(Token {
+                    line,
+                    token_type: TokenType::LeftAngle,
+                }),
+
+                // if the character is +
+                '+' => Some(Token {
+                    line,
+                    token_type: TokenType::Plus,
+                }),
+
+                // if the character is -
+                '-' => Some(Token {
+                    line,
+                    token_type: TokenType::Minus,
+                }),
+
+                // if the character is ]
+                ']' => Some(Token {
+                    line,
+                    token_type: TokenType::RightSquare,
+                }),
+
+                // if the character is [
+                '[' => Some(Token {
+                    line,
+                    token_type: TokenType::LeftSquare,
+                }),
+
+                // if the character is ,
+                ',' => Some(Token {
+                    line,
+                    token_type: TokenType::Comma,
+                }),
+
+                // if the character is .
+                '.' => Some(Token {
+                    line,
+                    token_type: TokenType::Dot,
+                }),
+
+                // if its a newline character, we increase line number count.
+                '\n' => {
+                    line += 1;
+                    None
+                }
+
+                // consider everything else as comments.
+                _ => None,
+            })
+            .collect()
+    }
 }
