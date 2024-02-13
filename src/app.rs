fabeb99b66634e8fe2e5c612c0a41543ec7c7a6e
Prashant <prashantrahul141@protonmail.com>
2024-02-14 02:46:40 +0530

    introducing app struct to handle top level stuff.

---
+++ src/app.rs
@@ -0,0 +1,40 @@
+use std::{self, env, fs, process::exit};
+
+use crate::lexer::Lexer;
+
+/// Top level App struct to control everything in one place.
+pub struct App;
+
+impl App {
+    // simple constructor.
+    pub fn new() -> Self {
+        Self
+    }
+
+    // reads file content, returns string of file content otherwise panics.
+    fn get_file_contents(filepath: &str) -> String {
+        let contents = fs::read_to_string(filepath);
+
+        if let Ok(file) = contents {
+            return file;
+        }
+
+        panic!("Failed to open file.")
+    }
+
+    // start the interpreter.
+    pub fn run(&self) {
+        if env::args().len() != 2 {
+            println!("BFIR - BrainF*ck Interpreter in Rust.\n\nUsage:\n    bfi [FILENAME]");
+            exit(0);
+        };
+
+        let file_content = App::get_file_contents(&env::args().collect::<Vec<String>>()[1]);
+
+        let lexer = Lexer::new(&file_content);
+
+        for token in lexer.tokens {
+            dbg!(token);
+        }
+    }
+}
