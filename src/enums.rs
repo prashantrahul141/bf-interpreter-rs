e2f0650c34ef88027fed800e4e211a6504de0524
Prashant <prashantrahul141@protonmail.com>
2024-02-15 00:26:49 +0530

    add opcodes and stmt enum

--- src/enums.rs
+++ src/enums.rs
@@ -19,3 +19,24 @@ pub struct Token {
     pub token_type: TokenType,
 }
 
+// Instruction set supported by the vm.
+#[derive(Debug)]
+pub enum OpCode {
+    // moves pointer one cell right.
+    MovePtrForward,
+    // moves pointer one cell left.
+    MovePtrBackward,
+    // reads one character from stdin.
+    ReadFromStdin,
+    // writes one character to stdout.
+    WriteToStdout,
+    // increment or decrement value at the ptr.
+    Crement(i8),
+}
+
+// statements supported by the vm.
+#[derive(Debug)]
+pub enum Stmt {
+    NodeStmt(OpCode),
+    WhileStmt(Vec<Stmt>),
+}
