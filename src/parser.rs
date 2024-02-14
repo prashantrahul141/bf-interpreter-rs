4d9a4aa8eff096bc7042b0b79d4a122395367fbb
Prashant <prashantrahul141@protonmail.com>
2024-02-15 00:30:21 +0530

    add parse_instruction function and public parse function

--- src/parser.rs
+++ src/parser.rs
@@ -18,7 +18,45 @@ impl<'a> Parser<'a> {
         }
     }
 
-    pub fn parse(&self) {}
+    pub fn parse(&mut self) {
+        while !matches!(self.lexer.peek().token_type, TokenType::Eof) {
+            if let Some(instruction) = self.parse_instruction() {
+                self.instructions.push(instruction);
+            }
+        }
+    }
+
+    fn parse_instruction(&mut self) -> Option<Stmt> {
+        let current = self.lexer.pop();
+
+        match current.token_type {
+            TokenType::RightAngle => Some(Stmt::NodeStmt(OpCode::MovePtrForward)),
+            TokenType::LeftAngle => Some(Stmt::NodeStmt(OpCode::MovePtrBackward)),
+            TokenType::Plus => Some(Stmt::NodeStmt(OpCode::Crement(1))),
+            TokenType::Minus => Some(Stmt::NodeStmt(OpCode::Crement(-1))),
+            TokenType::Comma => Some(Stmt::NodeStmt(OpCode::ReadFromStdin)),
+            TokenType::Dot => Some(Stmt::NodeStmt(OpCode::WriteToStdout)),
+            TokenType::LeftSquare => {
+                let mut inner_instructions: Vec<Stmt> = vec![];
+
+                while !self.match_token(TokenType::RightSquare) {
+                    if let Some(instruction) = self.parse_instruction() {
+                        inner_instructions.push(instruction);
+                    }
+                }
+
+                // consuming ']'
+                self.lexer.pop();
+
+                Some(Stmt::WhileStmt(inner_instructions))
+            }
+            TokenType::RightSquare => {
+                Parser::error("Unexpected ']'", &current);
+                None
+            }
+            TokenType::Eof => None,
+        }
+    }
 
     fn is_at_end(&self) -> bool {
         self.lexer.tokens.len() == 0
