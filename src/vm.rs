5470d94fa7711d380b2077a87964b291b3167b4c
Prashant <prashantrahul141@protonmail.com>
2024-02-15 21:50:45 +0530

    implement '[' for vm and abstract node stuff

--- src/vm.rs
+++ src/vm.rs
@@ -20,17 +20,37 @@ impl<'a> Vm<'a> {
     // Public run method to start executon
     pub fn run(&mut self) {
         for instruction in self.instructions {
-            match instruction {
-                Stmt::NodeStmt(node) => self.run_node(node),
-                Stmt::WhileStmt(_) => todo!(),
+            self.run_stmt(instruction);
+        }
+    }
+
+    //  runs statements.
+    fn run_stmt(&mut self, stmt: &Stmt) {
+        match stmt {
+            Stmt::NodeStmt(node) => self.run_opcode(node),
+            Stmt::WhileStmt(node) => self.while_stmt(node),
+        }
+    }
+
+    // while statements
+    fn while_stmt(&mut self, node: &Vec<Stmt>) {
+        while self.data[self.pointer] != 0 {
+            for stmt in node {
+                self.run_stmt(stmt);
             }
         }
     }
 
-    fn run_node(&mut self, node: &OpCode) {
+    // runs opcodes to vm.
+    fn run_opcode(&mut self, node: &OpCode) {
         match node {
+            // moves pointer forward.
             OpCode::MovePtrForward => self.pointer += 1,
+
+            // moves pointer backward.
             OpCode::MovePtrBackward => self.pointer -= 1,
+
+            // reads input from stdin and writes to current pointed cell.
             OpCode::ReadFromStdin => {
                 let mut input = String::new();
                 std::io::stdin().read_line(&mut input).unwrap_or(0);
@@ -38,12 +58,15 @@ impl<'a> Vm<'a> {
                 self.data[self.pointer] = first_char;
             }
 
+            // writes current cell as char to stdout.
             OpCode::WriteToStdout => {
                 print!(
                     "{}",
                     std::char::from_u32(self.data[self.pointer] as u32).unwrap_or('0')
                 )
             }
+
+            // increments or decrements current pointed value.
             OpCode::Crement(value) => {
                 self.data[self.pointer] = (self.data[self.pointer] as i8 + value) as u8;
             }
