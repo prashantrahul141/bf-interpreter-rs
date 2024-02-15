1fc346a3bb66a1b87cac95c284620180f76ff754
Prashant <prashantrahul141@protonmail.com>
2024-02-15 21:25:15 +0530

    impl run_node function for vm

--- src/vm.rs
+++ src/vm.rs
@@ -27,4 +27,27 @@ impl<'a> Vm<'a> {
         }
     }
 
+    fn run_node(&mut self, node: &OpCode) {
+        match node {
+            OpCode::MovePtrForward => self.pointer += 1,
+            OpCode::MovePtrBackward => self.pointer -= 1,
+            OpCode::ReadFromStdin => {
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).unwrap_or(0);
+                let first_char = input.bytes().nth(0).unwrap_or(0);
+                self.data[self.pointer] = first_char;
+            }
+
+            OpCode::WriteToStdout => {
+                print!(
+                    "{}",
+                    std::char::from_u32(self.data[self.pointer] as u32).unwrap_or('0')
+                )
+            }
+            OpCode::Crement(value) => {
+                self.data[self.pointer] = (self.data[self.pointer] as i8 + value) as u8;
+            }
+        }
+
+    }
 }
